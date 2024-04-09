e432bbcb771b0b44baf9ff92c5ae99cac3dfaab9
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-04-09 15:44:53 +0600

    classList Style

--- js/style.js
+++ js/style.js
@@ -3,4 +3,12 @@ console.log(document);
 const sections=document.querySelectorAll("section");
 for(const section of sections){
     section.style.border="2px solid red";
+    section.style.marginTop="5px";
+    section.style.borderRadius="15px";
+    section.style.paddingLeft="7px";
+    section.style.paddingRight="7px";
 }
+
+const newStyle=document.getElementById('first').classList;
+
+newStyle.add('title');
